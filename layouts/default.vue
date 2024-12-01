3195b7f25995a7e5515433118a42391bcc38aa5d
VloneCoder <mdjdmusique@gmail.com>
2024-12-01 21:13:04 +0300

    nuxt issue
    nuxt issue

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Debugging before slot</h1>
+    <slot />
+    <p>Debugging after slot</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
